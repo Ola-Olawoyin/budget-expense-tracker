a2c7a416726f15fc76489f5330735af8730b2e88
ola <oola.olawoyin@gmail.com>
2026-01-02 21:53:23 +0000

    add summary displays and expense table

--- js/script.js
+++ js/script.js
@@ -4,3 +4,21 @@ let budgetData = JSON.parse(localStorage.getItem("budgetData")) || {
     budgetLeft: 0,
     expenses: []
 };
+
+function updateUI() {
+    document.getElementById('totalBudget').textContent = budgetData.totalBudget.toFixed(2);
+    document.getElementById('totalExpenses').textContent = budgetData.totalExpenses.toFixed(2);
+    document.getElementById('budgetLeft').textContent = budgetData.budgetLeft.toFixed(2);
+
+    let tableBody = document.querySelector('table-container tbody');
+    tableBody.innerHTML = '';
+    budgetData.expenses.forEach(function (expense) {
+         let row = document.createElement('tr');
+         row.innerHTML = `
+         <td>${expense.title}</td>
+         <td>${expense.amount.toFixed(2)}</td>
+        <td><button class="btn btn-sm btn-danger">Remove</button></td>
+        `;
+        tableBody.appendChild(row)
+    });
+}
